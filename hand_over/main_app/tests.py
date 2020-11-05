368396e0f0bbc22d6b4544df7837cfb41b59e8bc
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2020-11-05 13:12:46 +0100

    basic tests for existing (4) views

--- hand_over/main_app/tests.py
+++ hand_over/main_app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+import pytest
+
 
 # Create your tests here.
+
+
+@pytest.mark.django_db
+def test_landing_page_url(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_donation_url(client):
+    response = client.get('/add-donation/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_login_url(client):
+    response = client.get('/login/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_register_url(client):
+    response = client.get('/register/')
+    assert response.status_code == 200
